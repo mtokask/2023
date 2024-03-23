525c20d720e60e3516fb21145fd428e304e82dbc
askmtok <maasu616@gmail.com>
2024-03-23 17:01:48 +0900

    rename work!!

--- 3c2501ff7e1cdb8606cdb148f4bb794a13ca13c4fcecbebd3241288b73428a14/assets/js/works.js
+++ 3c2501ff7e1cdb8606cdb148f4bb794a13ca13c4fcecbebd3241288b73428a14/assets/js/works.js
@@ -1,14 +1,14 @@
 "use strict";
-const worksItemArr = document.querySelectorAll('.works-item');
-for(let i = 0; worksItemArr.length; i++){
-  const ttlArr = document.querySelectorAll('.works-item .ttl');
-  const maskArr = document.querySelectorAll('.works-item .mask');
-  worksItemArr[i].addEventListener('mouseover', () => {
-    ttlArr[i].classList.add('is-show');
-    maskArr[i].classList.add('is-show');
+const worksItemArr = document.querySelectorAll(".work-item");
+for (let i = 0; worksItemArr.length; i++) {
+  const ttlArr = document.querySelectorAll(".work-item .ttl");
+  const maskArr = document.querySelectorAll(".work-item .mask");
+  worksItemArr[i].addEventListener("mouseover", () => {
+    ttlArr[i].classList.add("is-show");
+    maskArr[i].classList.add("is-show");
   });
-  worksItemArr[i].addEventListener('mouseout', () => {
-    ttlArr[i].classList.remove('is-show');
-    maskArr[i].classList.remove('is-show');
+  worksItemArr[i].addEventListener("mouseout", () => {
+    ttlArr[i].classList.remove("is-show");
+    maskArr[i].classList.remove("is-show");
   });
 }
